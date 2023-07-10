5809c7a5a0cd8b2114b1df57ddb35e65abb833cf
ianmacdougald <ian@tartanltd.com>
2023-07-10 13:14:54 -0400

    Add types for fibonacci JS function in middleware

--- demo/src/server/controllers/fibController.ts
+++ demo/src/server/controllers/fibController.ts
@@ -8,9 +8,9 @@ type fibInstance = {
 const instance: Promise<fibInstance> =
   require('../../../wasm-modules/fibonacci.js')();
 
-function fibonacci(element: number) {
-  const sequence = [0, 1];
-  for (let i = 2; i <= element; i++) {
+function fibonacci(element: number): number {
+  const sequence: Array<number> = [0, 1];
+  for (let i: number = 2; i <= element; i++) {
     sequence[i] = sequence[i - 2] + sequence[i - 1];
   }
   return sequence[element];
