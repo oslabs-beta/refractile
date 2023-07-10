d5d77de4f948f0562dfc0bfac3c13ca96f8779a1
Angelo Dell Acqua <angelo@angedell.it>
2023-07-10 15:13:29 -0400

    Demo page

--- demo/src/client/Components/DemoContainer.tsx
+++ demo/src/client/Components/DemoContainer.tsx
@@ -5,47 +5,6 @@ const DemoContainer = (props: any): JSX.Element => {
   return (
     <div className="container mx-auto py-20 overflow-auto">
       <FibJS></FibJS>
-
-      <p>
-        Aliquam erat volutpat. Pellentesque metus lorem, consectetur quis
-        ultrices ac, rutrum quis eros. In at ipsum bibendum, tempus nibh eget,
-        posuere dolor. Suspendisse nulla ligula, finibus aliquet nibh
-        sollicitudin, scelerisque ullamcorper sem. Etiam vitae placerat massa.
-        Proin diam nunc, varius id sagittis sed, commodo et diam. Fusce eu
-        tellus eros. Aliquam vel massa mollis, feugiat ligula eu, porta orci.
-        Nunc massa nunc, tristique ut facilisis nec, venenatis in nibh. Donec
-        tempus malesuada consectetur.
-      </p>
-      <p>
-        Aliquam erat volutpat. Pellentesque metus lorem, consectetur quis
-        ultrices ac, rutrum quis eros. In at ipsum bibendum, tempus nibh eget,
-        posuere dolor. Suspendisse nulla ligula, finibus aliquet nibh
-        sollicitudin, scelerisque ullamcorper sem. Etiam vitae placerat massa.
-        Proin diam nunc, varius id sagittis sed, commodo et diam. Fusce eu
-        tellus eros. Aliquam vel massa mollis, feugiat ligula eu, porta orci.
-        Nunc massa nunc, tristique ut facilisis nec, venenatis in nibh. Donec
-        tempus malesuada consectetur.
-      </p>
-      <p>
-        Aliquam erat volutpat. Pellentesque metus lorem, consectetur quis
-        ultrices ac, rutrum quis eros. In at ipsum bibendum, tempus nibh eget,
-        posuere dolor. Suspendisse nulla ligula, finibus aliquet nibh
-        sollicitudin, scelerisque ullamcorper sem. Etiam vitae placerat massa.
-        Proin diam nunc, varius id sagittis sed, commodo et diam. Fusce eu
-        tellus eros. Aliquam vel massa mollis, feugiat ligula eu, porta orci.
-        Nunc massa nunc, tristique ut facilisis nec, venenatis in nibh. Donec
-        tempus malesuada consectetur.
-      </p>
-      <p>
-        Aliquam erat volutpat. Pellentesque metus lorem, consectetur quis
-        ultrices ac, rutrum quis eros. In at ipsum bibendum, tempus nibh eget,
-        posuere dolor. Suspendisse nulla ligula, finibus aliquet nibh
-        sollicitudin, scelerisque ullamcorper sem. Etiam vitae placerat massa.
-        Proin diam nunc, varius id sagittis sed, commodo et diam. Fusce eu
-        tellus eros. Aliquam vel massa mollis, feugiat ligula eu, porta orci.
-        Nunc massa nunc, tristique ut facilisis nec, venenatis in nibh. Donec
-        tempus malesuada consectetur.
-      </p>
     </div>
   );
 };
--- demo/src/client/Components/FibJS.tsx
+++ demo/src/client/Components/FibJS.tsx
@@ -1,43 +1,75 @@
-import React, { useState, useEffect } from "react";
-import { FibJSProps, FibResult } from "../../types";
-
+import React, { useState, useEffect } from 'react';
+import { FibJSProps, FibResult } from '../../types';
 
 const FibJS = (props: FibJSProps): JSX.Element => {
-
-  const [fibFetched, setFibFetched] = useState<boolean>(false)
-  const [fibResult, setFibResult] = useState<number>(0)
-  const [fibTime, setFibTime] = useState<number>(0)
+  const [fibFetched, setFibFetched] = useState<boolean>(false);
+  const [isloading, setIsLoading] = useState<boolean>(false);
+  const [fibInput, setFibInput] = useState<number>(41);
+  const [fibResult, setFibResult] = useState<number>(0);
+  const [fibTime, setFibTime] = useState<number>(0);
 
   const handleClick = async () => {
-    const start: number = Date.now()
-    const response = await fetch('/api/fib-js', {
-      method: 'GET',
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-    const data = await response.json()
-    console.log(data)
-
-    setFibResult(data.result)
-    const timeTaken: number = Date.now() - start
-    setFibTime(timeTaken)
-    setFibFetched(true)
-  }
-
+    setIsLoading(true);
+    setFibFetched(false);
+    const start: number = Date.now();
+    try {
+      const response = await fetch('/api/fib-js/' + fibInput, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      if (response.ok) {
+        const data = await response.json();
+        setFibResult(data.result);
+        const timeTaken: number = Date.now() - start;
+        setFibTime(timeTaken);
+        setIsLoading(false);
+        setFibFetched(true);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
   return (
-    <div>
-      <h3>Click the button to calculate a big Fibonacci sequence in JavaScript</h3>
-      <button onClick={handleClick}>Run Fibonacci(50)</button>
-      {fibFetched &&
-        <div>
-          <p>{`The result of JS Fibonacci is: ${fibResult}`}</p>
-          <p>{`The time it took is: ${fibTime / 1000} seconds`}</p>
-        </div>
-      }
-    </div>
-  )
-}
+    <div className="flex-col gap-3">
+      <h3>
+        Click the button to calculate a big Fibonacci sequence in JavaScript
+      </h3>
+      <div className="flex gap-2">
+        <input
+          type="text"
+          placeholder="50"
+          value={fibInput}
+          onChange={(e) => setFibInput(Number(e.target.value))}
+          className="input input-bordered w-full max-w-xs"
+        />
+        <button className="btn btn-primary" onClick={handleClick}>
+          Run Fibonacci
+        </button>
+      </div>
 
-export default FibJS
+      <div className="py-4">
+        {isloading && (
+          <>
+            <div role="status" className="max-w-sm animate-pulse">
+              <div className="h-2 bg-primary rounded-full dark:bg-gray-700 max-w-[360px] mb-2.5"></div>
+              <div className="h-2.5 bg-primary rounded-full dark:bg-gray-700 w-48 mb-4"></div>
+              <span className="sr-only">Loading...</span>
+            </div>
+          </>
+        )}
+
+        {fibFetched && (
+          <>
+            <p>{`The result of JS Fibonacci is: ${fibResult}`}</p>
+            <p>{`The time it took is: ${fibTime / 1000} seconds`}</p>
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default FibJS;
