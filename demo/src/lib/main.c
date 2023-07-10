9fd30420e447ebfde2770ac8a531940546569590
ianmacdougald <ian@tartanltd.com>
2023-07-10 11:26:53 -0400

    Update types dependency

--- demo/src/lib/main.c
+++ demo/src/lib/main.c
@@ -8,14 +8,14 @@ unsigned long long fib(int n)
     return fib(n - 1) + fib(n - 2);
 }
  
-int main(int argc, char* argv[argc+1])
-{
-    if (argc <= 1) { 
-      printf("You fucked it up!\n"); 
-      return -1;
-    }
+// int main(int argc, char* argv[argc+1])
+// {
+//     if (argc <= 1) { 
+//       printf("You fucked it up!\n"); 
+//       return -1;
+//     }
 
-    int arg = atoi(argv[1]);
-    printf("%dth Fibonacci Number: %llu\n", arg, fib(arg));
-    return 0;
-}
+//     int arg = atoi(argv[1]);
+//     printf("%dth Fibonacci Number: %llu\n", arg, fib(arg));
+//     return 0;
+// }
