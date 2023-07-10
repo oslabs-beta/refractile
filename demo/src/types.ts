23e73a7f49db47d3eb6f6677cd6ad07c10955844
ianmacdougald <ian@tartanltd.com>
2023-07-10 14:20:08 -0400

    Add type for express middleware

--- demo/src/types.ts
+++ demo/src/types.ts
@@ -1,19 +1,23 @@
-export type FibJSProps = {
+import { Request, Response, NextFunction, RequestHandler } from 'express';
 
-}
+export type FibJSProps = {};
 
-export type FibOtherProps = {
-
-}
+export type FibOtherProps = {};
 
 export type FibResult = {
-  result: number
-}
+  result: number;
+};
 
 export type ServerError = {
-  log: string,
-  status: number,
+  log: string;
+  status: number;
   message: {
-    err: string
-  }
-}
+    err: string;
+  };
+};
+
+export type ExpressMWare = (
+  req: Response,
+  res: Response,
+  next: NextFunction
+) => void;
