457ddee4ec8e85bb9b8f6e1a8c1cdd5cb897aba7
ianmacdougald <ian@tartanltd.com>
2023-07-11 20:00:11 -0400

    Load wasm module from config path if refract is passed string

--- demo/refractile/refractile.ts
+++ demo/refractile/refractile.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
+const path = require('path');
 
 function refract(
   wasmSource: string | (() => Promise<any>),
@@ -6,10 +7,14 @@ function refract(
 ): (req: Request, res: Response, next: NextFunction) => Promise<any> {
   let instance: any;
 
-  if (typeof wasmSource === 'string') instance = require(wasmSource)();
+  if (typeof wasmSource === 'string') {
+    const wasmDir = require('../refractile.config.js').directory;
+    instance = require(`${wasmDir}/${wasmSource}.js`)();
+  }
 
-  return async (req: Request, res: Response, next: NextFunction) =>
-    (await instance)[method]();
+  return async (req: Request, res: Response, next: NextFunction) => {
+    (await instance)[method](req, res, next);
+  };
 }
 
 export default refract;
