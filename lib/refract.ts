ac2d6509ad88d66eef1112b110b5e22eb4fbd3bd
ianmacdougald <ian@tartanltd.com>
2023-07-17 17:02:38 -0400

    Add support for preload_cmds

--- lib/refract.ts
+++ lib/refract.ts
@@ -9,7 +9,10 @@ type RefractileConfigModule = {
 };
 
 type RefractileConfig = {
-  [module: string]: RefractileConfigModule;
+  preload_cmds: Array<string>;
+  modules: {
+    [module: string]: RefractileConfigModule;
+  };
 };
 
 type ExpressMWare = (req: Request, res: Response, next: NextFunction) => void;
@@ -18,7 +21,10 @@ type RefractInstance = {
 };
 
 let dir: string | null;
-let config: RefractileConfig = (() => {
+let config: RefractileConfig;
+
+// Load config
+(() => {
   const packageName = require('../package.json').name;
   // Get the current working directory
 
@@ -38,11 +44,21 @@ let config: RefractileConfig = (() => {
 
   // Once the current directory has been found, load the configuration file
   try {
-    return require(path.resolve(dir, 'refractile.config.js'));
+    config = require(path.resolve(dir, 'refractile.config.js'));
   } catch (e) {
     throw Error('Error: problem loading refractile.config.js -- ' + e);
   }
+
+  if (Array.isArray(config['preload_cmds']))
+    config['preload_cmds'].forEach((cmd) => {
+      try {
+        execSync(cmd);
+      } catch (e) {
+        throw Error('Error: problem running preload_cmd: ' + cmd);
+      }
+    });
 })();
+
 function refract(
   src: string | (() => Promise<RefractInstance>),
   method: string
@@ -50,7 +66,7 @@ function refract(
   let instance: Promise<RefractInstance>;
   if (typeof src === 'string') {
     // 1.0 look up configuration
-    if (!config[src])
+    if (!config.modules || !config.modules[src])
       throw Error(
         'No configuration found for module ' +
           src +
@@ -58,7 +74,7 @@ function refract(
           dir
       );
     // 2.0 check module destination location
-    if (!config[src]['bin'])
+    if (!config.modules[src]['bin'])
       throw Error(
         'No bin destination found for module ' +
           '. Check configuration at ' +
@@ -66,8 +82,8 @@ function refract(
       );
 
     // If there is no file in bin, try to make it
-    if (!fs.existsSync(path.resolve(config[src]['bin'], `${src}.js`))) {
-      if (!config[src]['make'])
+    if (!fs.existsSync(path.resolve(config.modules[src]['bin'], `${src}.js`))) {
+      if (!config.modules[src]['make'])
         throw Error(
           'No make formula found for module ' +
             '. Check configuration at ' +
@@ -75,15 +91,15 @@ function refract(
         );
 
       // Build it
-      execSync(config[src]['make']);
+      execSync(config.modules[src]['make']);
 
       // Check that it was made
-      if (!fs.existsSync(path.resolve(config[src]['bin'], `${src}.js`)))
+      if (!fs.existsSync(path.resolve(config.modules[src]['bin'], `${src}.js`)))
         throw Error('Failed to build ' + src);
     }
 
     // Load it
-    instance = require(path.resolve(config[src]['bin'], `${src}.js`))();
+    instance = require(path.resolve(config.modules[src]['bin'], `${src}.js`))();
   } else if (typeof src === 'function') {
     // 2.0 load the instance from the function
     instance = src();
